3f7d48219823f33ccb7acd5f93c79a0c0f5ba246
root <root@ip-172-31-37-173.ap-south-1.compute.internal>
2020-02-04 06:24:06 +0000

    Email update on Prow Jobs1

--- call_ci.sh
+++ call_ci.sh
@@ -21,6 +21,16 @@ modify_template(){
 	sed -i "s/SPECTRO_BRANCH_NAME/$JOB_NAME/"  $template_name
  	sed -i "s/SPECTRO_BUILD_ID/$BUILD_ID/"  $template_name
 }
+send_mail() {
+	export SENDER
+	export RECEIVER
+	export MAIL_ATTACHMENT
+	export MAIL_FILE
+	export CC_USER
+	modify_template ${MAIL_FILE}
+	make build
+	bin/main 
+}
 
 
 true
@@ -28,6 +38,7 @@ if [[ $? -ne 0 ]]
 then
 	MAIL_FILE=template11.html
 	MAIL_ATTACHMENT=build.log	
+	send_mail
 fi
 
 if [[ $JOB_TYPE == periodic ]]
@@ -35,15 +46,8 @@ then
 	echo "hai" > manifest.yaml
 	MAIL_FILE=template2.html
 	MAIL_ATTACHMENT=manifest.yaml
+	send_mail
 fi	
 		
 
-export SENDER
-export RECEIVER
-export MAIL_ATTACHMENT
-export MAIL_FILE
-export CC_USER
-modify_template ${MAIL_FILE}
-make build
-bin/main 
 exit 0
